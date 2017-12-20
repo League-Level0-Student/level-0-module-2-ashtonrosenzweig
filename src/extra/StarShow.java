0d160595e0dad5c0d97e8553c8aa70e124da3831
Rosenzweig.Ashton <Rosenzweig.Ashton@SEMS-259-32.SEMS259.LAB>
2017-12-20 14:55:43 -0800

    RockPaperSissors, StarShow, IntroToRandom

--- src/extra/StarShow.java
+++ src/extra/StarShow.java
@@ -9,50 +9,52 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class StarShow {
 	
-	Robot robot = new Robot("batman");
+	Robot rob = new Robot("batman");
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
-
+rob.setSpeed(100);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+int robotX = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+int robotY = 950;
 		// 8. Make a variable to hold the star size and set it to 25
-
+int starSize = 25;
 		// 12. Repeat the steps #19 to #18, 30 times
-
+for (int i = 0; i < 30; i++) {
+	
 			// 19. Set the pen width to i 
-	
+rob.setPenWidth(i);
 			// 10. Set the X position of the robot to your X variable
-	
+rob.setX(robotX);
 			// 11. Set the Y position of the robot to your Y variable
-	
+rob.setY(robotY);
 			// 9. Call the drawStar() method with your star size variable
-	
+drawStar(starSize);
 			// 14. Increase the X position by star size. See Figure 2.
-	
+robotX = robotX + starSize;
 			// 15. decrease the Y position by star size. See Figure 3.
-	
+robotY = robotY - starSize;
 			// 16. Increase the star size by 20
-	
+starSize = starSize + 20;
 			// 17. Turn the robot 12 degrees
-	
+rob.turn(12);
 			// 18. Make each star a different random color like in Figure 4. 
-
+rob.setRandomPenColor();
 	}
-
+	}
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+rob.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
+for (int i = 0; i < 5; i++) {
 
 			// 1. Move the robot the distance of the starSize variable
-	
+rob.move(starSize);
 			// 3. Turn the robot 144 degrees
-			
+rob.turn(144);
+	}
 	}
 	
 	public static void main(String[] args) {
--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -16,11 +16,12 @@ public class IntroToRandom {
 		//2. Now make r random. 
 		//   Create an object of the Random class
 		//   Hint: new Random().nextInt()
-
+Random gen = new Random();
+r = gen.nextInt();
 		//3. Limit the random number between 0 and 100
-
+r = gen.nextInt(101);
 		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
-
+r = gen.nextInt(51) + 25;
 		//5. Challenge: Limit the random number between -222 and 88
 
 
