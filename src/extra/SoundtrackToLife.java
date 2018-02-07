9db886175a0eb77ae989ecdbb34f70ca627a7455
Rosenzweig.Ashton <Rosenzweig.Ashton@SEMS-259-32.SEMS259.LAB>
2018-02-07 14:49:57 -0800

    Soundtrack to Life

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -13,32 +13,30 @@ public class SoundtrackToLife {
 		// 1. Adjust this pop-up to find out what mood the user is in.
 		int userMood = JOptionPane.showOptionDialog(null, "What kind of mood are you in?", "Pop-up Title", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Happy", "Sad", "Angry" }, null);
-		
+
 		// 2. Their answer is stored in the userMood variable. Print it out.
-System.out.println(userMood);
-		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
+		System.out.println(userMood);
+		// 3. If they are in a stressed mood, use the playVideo method to play a calming
+		// song from YouTube.
 
-		
 		// 4. Play different songs for other moods.
-if(userMood == 0) {
-playVideo("https://www.youtube.com/watch?v=y6Sxv-sUYtM");
-}
+		if (userMood == 0) {
+			playVideo("https://www.youtube.com/watch?v=y6Sxv-sUYtM");
+		}
 
-if(userMood == 1) {
-playVideo("https://www.youtube.com/watch?v=hLQl3WQQoQ0");
-}
+		if (userMood == 1) {
+			playVideo("https://www.youtube.com/watch?v=hLQl3WQQoQ0");
+		}
 
-if(userMood == 2) {
-playVideo("https://www.youtube.com/watch?v=hTWKbfoikeg");
-}
-/**
-* If you can’t think of any, here are some you can use...
-*	http://bit.ly/video-for-happy
-*	http://bit.ly/video-for-sad
-*	http://bit.ly/video-for-angry
-**/
-		
+		if (userMood == 2) {
+			playVideo("https://www.youtube.com/watch?v=hTWKbfoikeg");
+		}
+		/**
+		 * If you can’t think of any, here are some you can use...
+		 * http://bit.ly/video-for-happy http://bit.ly/video-for-sad
+		 * http://bit.ly/video-for-angry
+		 **/
+
 		// If you are seeing ads at the beginning of your videos, install Adblock.
 
 	}
@@ -52,6 +50,3 @@ playVideo("https://www.youtube.com/watch?v=hTWKbfoikeg");
 		}
 	}
 }
-
-
-
